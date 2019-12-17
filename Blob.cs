1f5524ccf0628eb2d3c58d34b8cfe2db60c13f0e
nilu92 <57625806+nilu92@users.noreply.github.com>
2019-12-17 21:34:41 +0100

    Add files via upload

---
+++ Blob.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeExamLibrary
+{
+    class Blob : I3DStorageObject
+    {
+        public double x;
+        public double z;
+        public double y;
+        public Blob(double side, double weight, string description, double maxDimension, bool isFragile)
+        {
+            x = y = z = side;
+            Area = side * side * 6;
+            Volume = side * side * side;
+            Weight = weight;
+            Description = description;
+            MaxDimension = maxDimension;
+            IsFragile = isFragile;
+
+
+        }
+
+        public string Description { get; set; }
+        public double Weight { get; set; }
+        public double Volume { get; set; }
+        public double Area { get; set; }
+        public double MaxDimension { get; set; }
+        public bool IsFragile { get; set; }
+
+
+        public void InsuranceValue()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
