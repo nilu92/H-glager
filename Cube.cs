94ffe9f3b807af672d1848f525c3b0cf996a9c16
nilu92 <57625806+nilu92@users.noreply.github.com>
2019-12-17 19:52:21 +0100

    Add files via upload

--- Cube.cs
+++ Cube.cs
@@ -6,16 +6,16 @@ namespace HomeExamLibrary
 {
     class Cube : I3DStorageObject
     {
-        public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public double Weight { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public double Volume { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public double Area { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public double MaxDimension { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool IsFragile { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Description { get; set; }
+        public double Weight { get; set; }
+        public double Volume { get; set; }
+        public double Area { get; set; }
+        public double MaxDimension { get; set; }
+        public bool IsFragile { get; set; }
 
         public void InsuranceValue()
         {
-            throw new NotImplementedException();
+           
         }
     }
 }
