c7d58fd26df1dd4c97b44e454f7e525f97002152
nilu92 <57625806+nilu92@users.noreply.github.com>
2019-12-17 22:58:23 +0100

    Add files via upload

--- WareHouse.cs
+++ WareHouse.cs
@@ -4,12 +4,23 @@ using System.Text;
 
 namespace HomeExamLibrary
 {
-   public class WareHouse
+    public class WareHouse
     {
-        public WareHouse() 
+        public WareHouse()
         {
-        
-        
+
+
+        }
+       
+        public void CreateBox()
+        {
+            // ska automatiskt lagra lådor
+            // Kunna lagra lådor på angiven plats
+           
+        }
+        public void SearchBox() 
+        {
+            //söka efter lådor med hjälp av Description.
         }
     
     }
