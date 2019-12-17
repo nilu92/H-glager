6531eac9cff4ad46852cfcc44bdf052438c8511a
nilu92 <57625806+nilu92@users.noreply.github.com>
2019-12-17 21:12:27 +0100

    Add files via upload

---
+++ Cubeoid.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeExamLibrary
+{
+    class Cubeoid : I3DStorageObject
+    {
+        public Cubeoid(double x, double y,double z,  double weight,double maxDimension, string description,bool isFragile) 
+        {
+            Area = 
+            Volume = 
+            Weight = weight;
+            MaxDimension = maxDimension;
+            Description = description;
+            IsFragile = isFragile;
+
+           
+        
+        }
+        public string Description { get; set; }
+        public double Weight { get; set; }
+        public double Volume { get ; set; }
+        public double Area { get; set; }
+        public double MaxDimension { get ; set ; }
+        public bool IsFragile { get; set ; }
+
+        public void InsuranceValue()
+        {
+         
+        }
+    }
+}
