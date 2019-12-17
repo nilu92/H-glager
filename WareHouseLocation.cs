d862b05c155db2b3de1d7b516a3fab2fafab5973
nilu92 <57625806+nilu92@users.noreply.github.com>
2019-12-17 22:57:46 +0100

    Add files via upload

---
+++ WareHouseLocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeExamLibrary
+{
+    class WareHouseLocation : WareHouse
+    {
+        public double MaxVolume;
+        public double MaxWeight;
+
+        public WareHouseLocation(double height, double width, double depth)
+        {
+            MaxVolume = height * width * depth;
+            MaxWeight = 1000;
+
+        }
+
+
+
+    }
+}
