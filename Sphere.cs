1f7d98b4e2fc54b78ac2d4d8e1dd2819f9c41c10
nilu92 <57625806+nilu92@users.noreply.github.com>
2019-12-17 21:24:09 +0100

    Add files via upload

---
+++ Sphere.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeExamLibrary
+{
+    class Sphere : I3DStorageObject
+    {
+
+        public Sphere(double radius, double weight, string description, double maxDimension, bool isFragile) 
+        {
+
+            Area = 4/3 * Math.PI * radius * radius * radius;
+            Volume = Math.PI * 4 * radius * radius;
+            Weight = weight;
+            MaxDimension = maxDimension;
+            Description = description;
+            IsFragile = isFragile;
+
+        }
+            
+        
+        public string Description { get; set; }
+        public double Weight { get; set; }
+        public double Volume { get; set; }
+        public double Area { get; set; }
+        public double MaxDimension { get; set; }
+        public bool IsFragile { get; set; }
+
+
+        public void InsuranceValue()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
