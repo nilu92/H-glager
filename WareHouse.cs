363daacd36fc6470eb7fa707d2a37cef3e0e0cd4
nilu92 <57625806+nilu92@users.noreply.github.com>
2019-12-18 15:00:33 +0100

    Add files via upload

--- WareHouse.cs
+++ WareHouse.cs
@@ -13,30 +13,32 @@ namespace HomeExamLibrary
                 new List<WareHouseLocation>(100),
                 new List<WareHouseLocation>(100),
                 new List<WareHouseLocation>(100),
+
+                
              };
        
-
-        public I3DStorageObject CreateCube(double side,double weight, string description, double maxDimension, bool isFragile) 
+            
+        public I3DStorageObject CreateCube(int iD,double side,double weight, string description, double maxDimension, bool isFragile) 
         {
-            Cube cube = new Cube(side,weight,description,maxDimension,isFragile);
+            Cube cube = new Cube(iD,side,weight,description,maxDimension,isFragile);
             return cube;
         }
         
-        public I3DStorageObject CreateCubeoid(double x, double y, double z, double weight,double maxDimension, string description,bool isFragile) 
+        public I3DStorageObject CreateCubeoid(int iD, double x, double y, double z, double weight,double maxDimension, string description,bool isFragile) 
         {
-            Cubeoid cubeoid = new Cubeoid(x,y,z,weight,maxDimension,description,isFragile);
+            Cubeoid cubeoid = new Cubeoid(iD,x,y,z,weight,maxDimension,description,isFragile);
             return cubeoid;
         }
        
-        public I3DStorageObject CreateSphere(double radius, double weight,string description,double maxDimension,bool isFragile) 
+        public I3DStorageObject CreateSphere(int iD, double radius, double weight,string description,double maxDimension,bool isFragile) 
         {
-            Sphere sphere = new Sphere(radius,weight,description,maxDimension,isFragile);
+            Sphere sphere = new Sphere(iD,radius,weight,description,maxDimension,isFragile);
             return sphere;
         }
 
-        public I3DStorageObject CreateBlob(double side, double weight, string description, double maxDimension) 
+        public I3DStorageObject CreateBlob(int iD, double side, double weight, string description, double maxDimension) 
         {
-            Blob blob = new Blob(side,weight,description,maxDimension);
+            Blob blob = new Blob(iD,side,weight,description,maxDimension);
             return blob;
         }
         
