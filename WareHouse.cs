9f526149f214591fd5ed60474ace39549388ca9b
nilu92 <57625806+nilu92@users.noreply.github.com>
2019-12-18 01:05:00 +0100

    Add files via upload

--- WareHouse.cs
+++ WareHouse.cs
@@ -6,21 +6,25 @@ namespace HomeExamLibrary
 {
     public class WareHouse
     {
+        public List<I3DStorageObject> Storage { get; }
         public WareHouse()
         {
 
-
+            Storage = new List<I3DStorageObject>();
         }
        
-        public void CreateBox()
+        public I3DStorageObject CreateBox(string Description)
         {
+            Storage storage = new I3DStorageObject();
+           //Cannot create an instance of the abstract class or interface 'I3DStorageObject'
             // ska automatiskt lagra lådor
             // Kunna lagra lådor på angiven plats
-           
+            return null;
         }
-        public void SearchBox() 
+        public void SearchForBox() 
         {
             //söka efter lådor med hjälp av Description.
+            
         }
     
     }
