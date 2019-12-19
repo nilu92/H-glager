b2beb0e93978118293281804e351052a9bbe3726
nilu92 <57625806+nilu92@users.noreply.github.com>
2019-12-19 20:50:22 +0000

    Add files via upload

--- WareHouseLocation.cs
+++ WareHouseLocation.cs
@@ -7,8 +7,9 @@ namespace HomeExamLibrary
     public class WareHouseLocation
     {
         public int FloorID { get; set; }
-        public List<I3DStorageObject> storage;
-       
+        public List<I3DStorageObject> storage = new List<I3DStorageObject>();
+
+
         public double MaxVolume;
         public double MaxWeight;
 
@@ -16,11 +17,27 @@ namespace HomeExamLibrary
         {
             MaxVolume = height * width * depth;
             MaxWeight = 1000;
-           
-           
         }
 
 
+        public bool hasAvailableVolumeForObject(I3DStorageObject s)
+        {
+            double currentVolume = 0;
 
+            foreach (I3DStorageObject obj in storage)
+            {
+                currentVolume += obj.Volume;
+            }
+
+            bool available = MaxVolume - currentVolume;
+            if (s.Volume <= available)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
