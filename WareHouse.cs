e8ea6d8fa479a1dbb2b51bf337dfa5b572cafeae
nilu92 <57625806+nilu92@users.noreply.github.com>
2020-06-03 22:11:25 +0200

    Add files via upload

--- WareHouse.cs
+++ WareHouse.cs
@@ -1,305 +1,197 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections;
 using System.Text;
-using System.Threading;
 using System.Linq;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using TentaBibliotek;
 
 
-namespace TentaBibliotek
+namespace HomeExamLibrary
 {
-    [Serializable]
-
-    public class WareHouse
-
+    public class WareHouse : ICloneable
     {
 
-
-        public int ID = 99;
-        WareHouseLocation[,] Höglager = new WareHouseLocation[4, 101];
-        public List<int> IDLåda { get; set; }
-        //public WareHouseLocation warehousetest = new WareHouseLocation();
-       
+        public List<WareHouseLocation> locations;
+         
+        private int iDCounter;
+        //WareHouse wareHouse = new WareHouse();
         public WareHouse()
         {
-
-            for (int floor = 1; floor < Höglager.GetLength(0); floor++)
+            
+            locations = new List<WareHouseLocation>();
+            
+            for (int i = 0; i < 100; i++)
             {
-                for (int collumn = 1; collumn < Höglager.GetLength(1); collumn++)
-                {
-                    Höglager[floor, collumn] = new WareHouseLocation();
-                }
-            }
-            this.ID = Få_ID();
+                WareHouseLocation whl = new WareHouseLocation(150, 150, 150);
+                locations.Add(whl);
+                whl.FloorID = 1;
 
-       
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                WareHouseLocation whl = new WareHouseLocation(150, 150, 150);
+                locations.Add(whl);
+                whl.FloorID = 2;
+
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                WareHouseLocation whl = new WareHouseLocation(150, 150, 150);
+                locations.Add(whl);
+                whl.FloorID = 3;
+            }
 
         }
-        public int Få_ID()
-        {
-            for (int floor = 1; floor < Höglager.GetLength(0); floor++)
-            {
-                for (int collumn = 1; collumn < Höglager.GetLength(1); collumn++)
-                {
-                    if (Höglager[floor, collumn].Boxes.Count == 0)
-                    {
-                        
-                    }
-                    else
-                    {
-                        foreach (I3DStorage box in Höglager[floor, collumn].Boxes)
-                        {
-                            this.ID++;
-                        }
-                    }
 
+       
+        public I3DStorageObject CreateCube(double side, double weight, string description, double maxDimension, bool isFragile)
+        {
+            Cube cube = new Cube(side, weight, description, maxDimension, isFragile);
+            cube.ID = iDCounter++;
+
+            return cube;
+        }
+
+        public I3DStorageObject CreateCubeoid(double x, double y, double z, double weight, double maxDimension, string description, bool isFragile)
+        {
+            Cubeoid cubeoid = new Cubeoid(x, y, z, weight, maxDimension, description, isFragile);
+            cubeoid.ID = iDCounter++;
+            return cubeoid;
+        }
+
+        public I3DStorageObject CreateSphere(double radius, double weight, string description, double maxDimension, bool isFragile)
+        {
+            Sphere sphere = new Sphere(radius, weight, description, maxDimension, isFragile);
+            sphere.ID = iDCounter++;
+
+            return sphere;
+        }
+
+        public I3DStorageObject CreateBlob(double side, double weight, string description, double maxDimension)
+        {
+            Blob blob = new Blob(side, weight, description, maxDimension);
+            blob.ID = iDCounter++;
+
+            return blob;
+        }
+
+
+        public bool AddStorageAuto(I3DStorageObject s)
+        {
+            foreach (WareHouseLocation wareHouselocation in locations)
+            {
+                bool available = wareHouselocation.hasAvailableVolumeForObject(s);
+                if (available)
+                {
+                    wareHouselocation.AddtoLocation(s);
+                  //  wareHouselocation.storage.Add(s);
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+
+        public bool AddStorageManual(I3DStorageObject s, int spot)
+        {
+            WareHouseLocation wareHouseLocation = locations[spot];
+            bool available = wareHouseLocation.hasAvailableVolumeForObject(s);
+            if (available)
+            {
+                wareHouseLocation.AddtoLocation(s);
+                return true;
+            }
+
+            return false;
+        }
+
+     /*  public int SearchSpot(int spot) 
+        {
+            WareHouseLocation whl = locations[spot];
+            for (int i = 0; i < spot; i++)
+            {
+                whl.Search(spot);
+                return spot;
+            }
+
+
+            return 0;
+        }
+        */
+        public I3DStorageObject Search(int id)
+        {
+            
+            
+            foreach (WareHouseLocation whl in locations)
+            {
+                I3DStorageObject s = whl.Search(id);
+                if(s != null) 
+                {
+                    return s;
+                }  
+             }
+            return null;
+        }
+
+        public int SearchSpot(int spot) 
+        {
+            foreach (var whl in locations)
+            {
+                for (int i = 0; i < whl.GetStorage().Count; i++)
+                {
+                    var box = whl.GetStorage()[i];
+                    if(box != null) 
+                    {
+                        return spot;
+                    }
                 }
             }
-            return this.ID + 1;
+            return -1;
+        }
+
+
+
+            public bool Contains(int id)
+            {
+                var box = Search(id);
+                return box != null;
+            }
+        
+        public bool Remove(I3DStorageObject s,int id)
+        {
+            foreach (WareHouseLocation whl in locations)
+            {
+                 whl.Remove(s,id);
+                        // whl.storage.Remove(obj);
+                        return true;
+            }
+
+            return false;
+        }
+        public bool Move(int id, int spot) 
+        {
+            foreach (WareHouseLocation wareHouseLocation in locations)
+            {
+                I3DStorageObject box = Search(id);
+                bool didRemove = Remove(box,id);
+                if (didRemove) 
+                {
+                    bool didPlace = AddStorageManual(box, spot);
+                     return didPlace;
+                }
+            }
+
+            return false;
         }
 
         
-
-
-
-        public int Konverteraochläggtillplats(UserInputLåda userLåda, int specifikplats, int våning = 0, int plats = 0)
+        public void CreateClone(WareHouseLocation whl) 
         {
-           int result = -1;
-            if (userLåda.Description == "Cube")
-            {
-                Cube cube = new Cube(userLåda.Side,this.ID, userLåda.Description, userLåda.Weight, userLåda.isFragile,userLåda.Insured);
-                if (specifikplats == 1)
-                {
-                    result = LäggtillSpecifikWareHouseLocation(cube, våning, plats);
-                }
-                else if (specifikplats == 2)
-                {
-                    result = läggadirekttillWareHouseLocation(cube);
-                }
-            }
-            if (userLåda.Description == "Blob")
-            {
-                Blob blob = new Blob(userLåda.Side, this.ID, userLåda.Weight, userLåda.Description,userLåda.Insured );
-                if (specifikplats == 1)
-                {
-                   result = LäggtillSpecifikWareHouseLocation(blob, våning, plats);
-                }
-                else if (specifikplats == 2)
-                {
-                    result = läggadirekttillWareHouseLocation(blob);
-                }
-            }
-            if (userLåda.Description == "Sphere")
-            {
-                Sphere sphere = new Sphere(userLåda.Side / 2, this.ID, userLåda.Weight, userLåda.Description, userLåda.isFragile,userLåda.Insured);
-                if (specifikplats == 1)
-                {
-                    result= LäggtillSpecifikWareHouseLocation(sphere, våning, plats);
-                }
-                else if (specifikplats == 2)
-                {
-                    result = läggadirekttillWareHouseLocation(sphere);
-                }
-            }
-            if (userLåda.Description == "Cubeoid")
-            {
-                Cubeoid cubeoid = new Cubeoid(userLåda.XSide, userLåda.YSide, userLåda.ZSide, this.ID, userLåda.Weight, userLåda.Description, userLåda.isFragile,userLåda.Insured);
-                if (specifikplats == 1)
-                {
-                    result = LäggtillSpecifikWareHouseLocation(cubeoid, våning, plats);
-                }
-                else if (specifikplats == 2)
-                {
-                    result = läggadirekttillWareHouseLocation(cubeoid);
-                }
-            }
-            if (result!=-1)
-            {
-                this.ID++;
-            }
-            return result;
+            whl.Clone();
         }
-        public  int LäggtillSpecifikWareHouseLocation(I3DStorage låda, int våning, int plats)
+        public object Clone()
         {
-            int result = -1;
-            if (Höglager[våning, plats].AddLådatolocation(låda)!=-1)
-            {
-                
-               result= låda.ID;
-            }
-            else if(Höglager[våning, plats].AddLådatolocation(låda)==-1)
-            {
-                result=-1;
-            }
-            return result;
-
-
-            
-
-            
-
-
+            throw new NotImplementedException();
         }
-        public void SkrivUT()
-        {
-
-
-            for (int floor = 1; floor < Höglager.GetLength(0); floor++)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Våning {0}", floor);
-                Console.ForegroundColor = ConsoleColor.White;
-
-                for (int collumn = 1; collumn < Höglager.GetLength(1); collumn++)
-                {
-                    Console.Write("Plats {0}: ", collumn);
-                    Höglager [floor, collumn].Content();
-                }
-                if (floor <= 2)
-                {
-                    Console.WriteLine("Tryck enter för nästa våning");
-                    Console.ReadKey();
-                    Console.Clear();
-                }
-
-            }
-
-
-        }
-        public int läggadirekttillWareHouseLocation(I3DStorage userLåda)
-        {
-            int result = -1;
-
-            for (int våning = 1; våning< Höglager.GetLength(0); våning++)
-            {
-                for (int plats = 1; plats < Höglager.GetLength(1); plats++)
-                {
-
-                    result = Höglager[våning, plats].AddLådatolocation(userLåda);
-                    if(result !=-1)
-                    {
-                        
-                        result= userLåda.ID;
-                        return result;
-                    }
-                }
-                //break;
-            }
-            return result;
-        }
-        public int[] HittaLådaviaID(int ID)
-        {
-            int[] index = new int[2];
-            index[0] = -1; 
-            int found; 
-            for (int våning = 1; våning < Höglager.GetLength(0); våning++)
-            {
-                for (int plats = 1; plats < Höglager.GetLength(1); plats++)
-                {
-                    found = Höglager[våning, plats].Hittalåda(ID);
-                    if (found != -1)
-                    {
-                        index[0] = våning;
-                        index[1] = plats;
-                        return index;
-                    }
-                }
-            }
-            return index; 
-        }
-        public int FlyttaLådaviaIdVåningPlats(int ID, int våning, int plats)
-        {
-            int result = -1;
-            int[] index = new int[2];
-             index = HittaLådaviaID(ID);
-            if (index[0] == -1)
-            {
-                
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Lådan som du vill flytta finns inte  ");
-                Console.WriteLine(" Bekräfta ID Lådan igen!! ");
-                Console.WriteLine(" Försöka igen  ");
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-            else
-            {
-                int indexplats = Höglager[index[0], index[1]].Hittalåda(ID);
-               I3DStorage lådatillflytta = Höglager[index[0], index[1]].Boxes[indexplats].Clone() as I3DStorage;
-                result = LäggtillSpecifikWareHouseLocation(lådatillflytta, våning, plats);
-                if (result!=-1)
-                {
-                    Höglager[index[0], index[1]].Tabort(indexplats);
-                }
-            }
-            return result;
-        }
-        public void TaBortLådaviaID(int ID)
-        {
-            int[] index = new int[2];
-
-            index = HittaLådaviaID(ID);
-            if (index[0] == -1)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Kunde inte hitta lådan att ta bort.");
-                Console.WriteLine(" Bekräfta ID Lådan igen!! ");
-                Console.WriteLine(" Försöka igen  ");
-                Console.ForegroundColor = ConsoleColor.White;
-
-            }
-            else
-            {
-                int indexLager = Höglager[index[0], index[1]].Hittalåda(ID);
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Lådan  Har tagit bort :\n {0}  ", Höglager[index[0], index[1]].Boxes[indexLager]);
-                Console.ForegroundColor = ConsoleColor.White;
-
-                Höglager[index[0], index[1]].Tabort(indexLager);
-                Console.ReadKey();
-            }
-
-        }
-        public object Clone(int våning, int plats)
-        {
-            WareHouseLocation copy = Höglager[våning, plats].Clone() as WareHouseLocation;
-            return copy;
-        }
-        public WareHouseLocation this[int index1, int index2]
-        {
-            get { return this.Höglager[index1, index2]; }
-            set { this.Höglager[index1, index2] = value; }
-        }
-        public void skrivinFile(WareHouse ObjectförskrinIn)
-        {
-            string Fileadress = "HÖLAGER.txt";
-            using (Stream str = File.Open(Fileadress, FileMode.Create))
-            {
-                var file = new BinaryFormatter();
-                file.Serialize(str, ObjectförskrinIn);
-                str.Close();
-            }
-        }
-        public WareHouse LäsaFile()
-        {
-            string Fileadress = "HÖLAGER.txt";
-            using (Stream str = File.Open(Fileadress, FileMode.OpenOrCreate))
-            {
-                var file = new BinaryFormatter();
-                
-                return file.Deserialize(str) as WareHouse;
-            }
-        }
-
-
-
-
-
-
-    }
+     }
 }
-       
-    
-
--- WareHouseLocation.cs
+++ WareHouseLocation.cs
@@ -1,140 +1,97 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
-namespace TentaBibliotek
+namespace HomeExamLibrary
 {
-    [Serializable]
-   public class WareHouseLocation : IEnumerable<I3DStorage>
+    public class WareHouseLocation : ICloneable
     {
-        
-        public List<I3DStorage> Boxes { get; set; }
+        public int FloorID { get; set; }
+        private List<I3DStorageObject> storage;
 
-        internal bool KanLagraFragile { get; set; }
-        public double Height { get; }
-        public double Width { get; }
-        public double Depth { get; }
-        public double MaxDimension { get; }
-        public double MaxVolume { get; }
-        public decimal MaxWeight { get; }
-        public WareHouseLocation(double height=200,double width=200,double depth=200)
-        {
-           this.Boxes = new List<I3DStorage>();
-            this.Height = height;
-            this.Width = width;
-            this.Depth = depth;
-            this.KanLagraFragile = true;
-            this.MaxDimension = 200;
-            this.MaxVolume  = height * width * depth;
-            this.MaxWeight = 1000;
-            
-            
-            
-            
-        }
-      public object Clone()
-        {
-            WareHouseLocation clone = new WareHouseLocation(this.Height, this.Width, this.Depth);
-            foreach (I3DStorage box in this.Boxes)
-            {
-                I3DStorage Copylåda = box.Clone() as I3DStorage;
-                clone.Boxes.Add(Copylåda);
-            }
-            return clone;
 
-        }
-       public void Content()
+        public double MaxVolume;
+        public double MaxWeight;
+
+        public WareHouseLocation(double height, double width, double depth)
         {
-            WareHouseLocation clone = this.Clone() as WareHouseLocation;
-            foreach (I3DStorage box in clone.Boxes)
-            {
-                Console.Write("| {0} ", box);
-            }
-            Console.WriteLine();
+            storage = new List<I3DStorageObject>();
+            
+            MaxVolume = height * width * depth;
+            MaxWeight = 1000;
         }
 
-
-        public int AddLådatolocation(I3DStorage Låda)
+        public bool AddtoLocation(I3DStorageObject s) 
         {
-           int result = -1;
-            decimal currentWeight = 0;
-            double currentVolume = 0;
-            if (Låda.IsFragile==true && this.KanLagraFragile==true )
-            {
-                Boxes.Add(Låda);
-                this.KanLagraFragile= false;
-
-                result = Låda.ID;
-                
-
-            }
-
-          
-            foreach (I3DStorage obj in this.Boxes)
-            {
-                currentWeight = currentWeight + obj.Weight;
-                currentVolume = currentVolume + obj.Volume;
-            }
-            if (currentWeight + Låda.Weight < this.MaxWeight && Låda.MaxDimension < this.MaxDimension && currentVolume < this.MaxVolume && Låda.IsFragile == false)
-            {
-                this.Boxes.Add(Låda);
-                this.KanLagraFragile = false;
-                result = Låda.ID;
-            }
-            return result;
-        }
-       public int Hittalåda(int id)
-        {
-            if (Boxes.Count == 0)
-            {
-
-            }
-            else
-            {
-                for (int i = 0; i < Boxes.Count; i++)
+            bool avaialable = hasAvailableVolumeForObject(s);
+                if(avaialable) 
                 {
-                    if (Boxes[i].ID == id)
-                    {
-                        return i;
-                    }
+                    storage.Add(s);
+                    return true;
                 }
-               
-            }
-
-            return -1; 
-
+            
+            return false;
         }
-     
-        public void Tabort(int index)
+       
+        public bool Remove(I3DStorageObject s, int id) 
         {
-            if (index == -1)
+            foreach (I3DStorageObject i3DStorageObject in storage)
             {
+                    storage.Remove(s);
+                    return true;
+             }
+            return false;
+        }
 
+        public I3DStorageObject Search(int id) 
+        {
+            foreach (I3DStorageObject i3DStorageObject in storage)
+            {
+                if(i3DStorageObject.ID == id) 
+                {
+                    return i3DStorageObject;
+                }
+            }
+            return null;
+        }
+
+       
+        public bool hasAvailableVolumeForObject(I3DStorageObject s)
+        {
+            double currentVolume = 0;
+            
+
+            foreach (I3DStorageObject obj in storage)
+            {
+                currentVolume += obj.Volume;
+            }
+          
+            double available = MaxVolume - currentVolume;
+            
+
+            if (s.Volume <= available)
+            {
+                return true;
             }
             else
             {
-                Boxes.RemoveAt(index);
+                return false;
             }
+        
+        }
+        public List<I3DStorageObject> GetStorage()
+        {
+            return storage;
         }
 
-        public IEnumerator<I3DStorage> GetEnumerator()
-        {
-            return this.Boxes.GetEnumerator();
-        }
 
-        IEnumerator IEnumerable.GetEnumerator()
+            public object Clone()
         {
-            return this.GetEnumerator();
-        }
-        public List<I3DStorage> ListOfContent()
-        {
-            List<I3DStorage> inhålletList = new List<I3DStorage>();
-            foreach (I3DStorage box in Boxes)
-            {
-                inhålletList.Add(box);
-            }
-            return inhålletList;
+            var copy = (WareHouseLocation)MemberwiseClone();
+            copy.storage = storage.Select(item => (I3DStorageObject)item.Clone()).ToList();
+
+            return copy;
         }
     }
 }
